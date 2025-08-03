46ea756ff47ea45d0421aaacb69cd3b255aa5a3e
Hana <hanamarwasadek2004@gmail.com>
2025-08-03 23:04:24 +0300

    solution of part02

---
+++ oop04/assignment/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop04.assignment
+{
+    internal class Employee
+    {
+
+        virtual public void Works()
+        {
+            Console.WriteLine("Employee is working");
+        }
+    }
+}
